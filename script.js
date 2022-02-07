e419204d3c31a487a264f82080351d82b3145a0b
Igor <example@gmail.com>
2022-02-07 22:12:16 +0300

    Created app 'Quiz' ver. 1.0

---
+++ script.js
@@ -0,0 +1,245 @@
+// находим все карточки и запонимаем данные ответов из каждой карточки
+const cards = [...document.querySelectorAll('.card')];
+
+// находим все progress-bar
+const progressSegments = document.querySelectorAll('.progress');
+
+// инициализируем хранилище;
+store.init(cards);
+
+// осуществляем вход в приложение
+main();
+
+// точка входа в приложение (отображаем первую карточку)
+function main() {
+	stepActive(1);
+	progressesUpdate(store.getProgressPercent());
+}
+
+function stepActive(number) {
+	// находим карточку
+	const card = document.querySelector(`.card[data-step="${number}"]`);
+
+	if (!card) {
+		return;
+	}
+
+	// отображаем активную карточку
+	for (const card of cards) {
+		card.classList.remove('card--active');
+	}
+
+	card.classList.add('card--active');
+
+	// проверяем инициализацию карточки и инициализируем ее при необходимости
+	if (card.dataset.inited) {
+		return;
+	}
+
+	card.dataset.inited = true;
+
+	if (number === 1) {
+		initStep_01();
+	} else if (number === 2) {
+		initStep_02();
+	} else if (number === 3) {
+		initStep_03();
+	} else if (number === 4) {
+		initStep_04();
+	} else if (number === 5) {
+		initStep_05();
+	} else if (number === 6) {
+		initStep_06();
+	}
+}
+
+// инициализация каждой отдельной карточки и логика ее работы
+function initStep_01() {
+	const card = document.querySelector(`.card[data-step="1"]`);
+	const startButton = card.querySelector('button[data-action="start"]');
+
+	startButton.addEventListener('click', () => stepActive(2));
+}
+
+function initStep_02() {
+	const card = document.querySelector(`.card[data-step="2"]`);
+	const toNextButton = card.querySelector('button[data-action="toNext"]');
+	const toPrevButton = card.querySelector('button[data-action="toPrev"]');
+	const variants = card.querySelectorAll('[data-value]');
+
+	toNextButton.disabled = true;
+
+	toNextButton.addEventListener('click', () => stepActive(3));
+	toPrevButton.addEventListener('click', () => stepActive(1));
+
+	for (const variant of variants) {
+		variant.addEventListener('click', variantClickHandler);
+	}
+
+	function variantClickHandler() {
+		const id = cards.indexOf(card);
+		const { value } = this.dataset;
+		store.dataUpdate(id, value);
+
+		for (const variant of variants) {
+			const radioButton = variant.querySelector('input[type="radio"]');
+			radioButton.checked = false;
+		}
+
+		const radioButton = this.querySelector('input[type="radio"]');
+		radioButton.checked = true;
+
+		toNextButton.disabled = false;
+
+		progressesUpdate(store.getProgressPercent());
+	}
+}
+
+function initStep_03() {
+	const card = document.querySelector(`.card[data-step="3"]`);
+	const toNextButton = card.querySelector('button[data-action="toNext"]');
+	const toPrevButton = card.querySelector('button[data-action="toPrev"]');
+	const variants = card.querySelectorAll('[data-value]');
+
+	toNextButton.disabled = true;
+
+	toNextButton.addEventListener('click', () => stepActive(4));
+	toPrevButton.addEventListener('click', () => stepActive(2));
+
+	for (const variant of variants) {
+		variant.addEventListener('click', variantClickHandler);
+	}
+
+	function variantClickHandler() {
+		const id = cards.indexOf(card);
+		const { value } = this.dataset;
+		const answers = store.dataUpdate(id, value);
+
+		for (const variant of variants) {
+			const { value } = variant.dataset;
+			const checkbox = variant.querySelector('input[type="checkbox"]');
+			checkbox.checked = Boolean(answers.includes(value));
+		}
+		
+		toNextButton.disabled = !Boolean(answers.length);
+
+		progressesUpdate(store.getProgressPercent());
+	}
+}
+
+function initStep_04() {
+	const card = document.querySelector(`.card[data-step="4"]`);
+	const toNextButton = card.querySelector('button[data-action="toNext"]');
+	const toPrevButton = card.querySelector('button[data-action="toPrev"]');
+	const variants = card.querySelectorAll('[data-value]');
+
+	toNextButton.disabled = true;
+
+	toNextButton.addEventListener('click', () => stepActive(5));
+	toPrevButton.addEventListener('click', () => stepActive(3));
+
+	for (const variant of variants) {
+		variant.addEventListener('click', variantClickHandler);
+	}
+
+	function variantClickHandler() {
+		const id = cards.indexOf(card);
+		const { value } = this.dataset;
+		const answers = store.dataUpdate(id, value);
+
+		for (const variant of variants) {
+			const { value } = variant.dataset;
+			if (answers.includes(value)) {
+				variant.classList.add('variant-square--active');
+			} else {
+				variant.classList.remove('variant-square--active');
+			}
+		}
+		
+		toNextButton.disabled = !Boolean(answers.length);
+
+		progressesUpdate(store.getProgressPercent());
+	}
+}
+
+function initStep_05() {
+	const card = document.querySelector('.card[data-step="5"]');
+	const toNextButton = card.querySelector('button[data-action="toNext"]');
+	const toPrevButton = card.querySelector('button[data-action="toPrev"]');
+
+	const nameInput = card.querySelector('input[data-field="name"]');
+	const surnameInput = card.querySelector('input[data-field="surname"]');
+	const emailInput = card.querySelector('input[data-field="email"]');
+
+
+	toNextButton.disabled = true;
+
+	toNextButton.addEventListener('click', () => stepActive(6));
+	toPrevButton.addEventListener('click', () => stepActive(4));
+
+	nameInput.addEventListener('keyup', nameKeyupHandler);
+	surnameInput.addEventListener('keyup', surnameKeyupHandler);
+	emailInput.addEventListener('keyup', emailKeyupHandler);
+
+	const id = cards.indexOf(card);
+
+	function nameKeyupHandler() {
+		const answers = store.dataUpdate(id, this.value, 'name');
+		nextButtonUpdate(answers);
+	}
+	
+	function surnameKeyupHandler() {
+		const answers = store.dataUpdate(id, this.value, 'surname');
+		nextButtonUpdate(answers);
+	}
+	
+	function emailKeyupHandler() {
+		const answers = store.dataUpdate(id, this.value, 'email');
+		nextButtonUpdate(answers);
+	}
+
+	function nextButtonUpdate(answers) {
+		let active = true;
+
+		if (!answers[0].name) {
+			active = false;
+		}
+
+		if (!answers[1].surname) {
+			active = false;
+		}
+
+		if (!answers[2].email) {
+			active = false;
+		}
+
+		toNextButton.disabled = !active;
+
+		progressesUpdate(store.getProgressPercent());
+	}
+}
+
+function initStep_06() {
+	const card = document.querySelector('.card[data-step="6"]');
+	const emailSpan = card.querySelector('span[data-field="email"]');
+
+	emailSpan.textContent = store.getEmail();
+}
+
+
+function progressesUpdate(progressPercent) {
+	for (const progressSegment of progressSegments) {
+		const progressElement = progressSegment.querySelector('progress');
+		const progressTitle = progressSegment.querySelector('.progress-title');
+
+		progressElement.value = progressPercent;
+		progressTitle.textContent = `${Math.floor(progressPercent)}%`;
+		progressTitle.style.width = `${progressPercent}%`;
+
+		if (progressPercent) {
+			progressTitle.style.display = 'block';
+		} else {
+			progressTitle.style.display = 'none';
+		}
+	}
+}
---
+++ store.js
@@ -0,0 +1,86 @@
+(() => {
+  const store = {};
+
+  let data = [];
+
+  const mailRe =
+	/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  store.init = (cards) => {
+    data = cards.map((card, id) => ({
+      id,
+      card,
+      answers: [],
+      progressValue: 0,
+    }));
+  };
+
+  store.dataUpdate = (id, value, field = null) => {
+    const item = data.find(item => item.id === id);
+
+    if (id === 1) {
+      item.answers.splice(0);
+      item.answers.push(value);
+      item.progressValue = 1;
+    } else if (id === 2 || id === 3) {
+      if (!item.answers.includes(value)) {
+        item.answers.push(value);
+        item.progressValue = 1;
+      } else {
+        const index = item.answers.indexOf(value);
+        item.answers.splice(index, 1);
+        item.progressValue = item.answers.length ? 1 : 0;
+      }
+    } else if (id === 4) {
+      if (!item.answers.length) {
+        item.answers.push(
+          {name: ''},
+          {surname: ''},
+          {email: ''}
+        );
+      }
+
+      for (const elem of item.answers) {
+        if (elem.hasOwnProperty(field)) {
+          if (field === 'email') {
+            elem[field] = mailRe.test(value) ? value : '';
+          } else {
+            elem[field] = value;
+          }
+          break;
+        }
+      }
+
+      item.progressValue = 0;
+      for (const elem of item.answers) {
+        const value = elem[Object.keys(elem)[0]];
+        if (value.length) {
+          item.progressValue += 1;
+        }
+      }
+    }
+
+    return getCopy(item.answers);
+  };
+
+  store.getProgressPercent = () => {
+    const sumProgressValues = data.map(item => item.progressValue)
+                                  .reduce((a, b) => a + b);
+    const progressPercent = sumProgressValues / 6 * 100;
+
+    return progressPercent;
+  };
+
+  store.getEmail = () => {
+    const email = data[4].answers[2].email;
+    return email;
+  };
+
+
+  window.store = store;
+
+  function getCopy(x) {
+    return JSON.parse(JSON.stringify(x));
+  }
+  
+})();
